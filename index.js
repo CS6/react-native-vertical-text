60fde2911e7d633351ca3cdb90eaec6b89af6018
sddivid <sduum82@gmail.com>
2019-04-13 17:32:51 +0800

    add  code

---
+++ index.js
@@ -0,0 +1,8 @@
+// import ProgressSteps from './src/ProgressSteps/ProgressSteps';
+// import ProgressStep from './src/ProgressSteps/ProgressStep';
+import VerticalText from './verticalText'
+
+// import { ProgressSteps, ProgressStep } from 'react-native-progress-steps';
+
+// export { ProgressSteps, ProgressStep };
+export { VerticalText};
